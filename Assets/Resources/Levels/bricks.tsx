e99cc71d0a82a0256377c7b544dd1ec31a6860f0
U-DESKTOP-FLC9L1M\dcioata <dcioata@luxoft.com>
2018-05-29 17:34:47 +0300

    added some resources

---
+++ Assets/Resources/Levels/bricks.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bricks" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../Textures/brick_brown.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../Textures/brick_red.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../Textures/brick_turquoise.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../Textures/brick_white.png"/>
+ </tile>
+</tileset>
---
+++ Assets/Scripts/EntityDefs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EntityDefs
+{
+    public enum BrickType : uint  // your custom enumeration
+    {
+        brick_none,
+        brick_brown, 
+        brick_red, 
+        brick_turquoise,
+        brick_white
+    };
+
+    private static EntityDefs instance=null;
+
+    private Dictionary <BrickType,string> m_materialMap = new Dictionary<BrickType,String>()
+    {
+        { BrickType.brick_brown,"brick_brown"},
+        { BrickType.brick_red,"brick_red"},
+        { BrickType.brick_turquoise,"brick_turquoise"},
+        { BrickType.brick_white,"brick_white"}
+
+    };
+    private Dictionary <BrickType,Material> m_materialList=new Dictionary<BrickType,Material>();
+
+    public static EntityDefs Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new EntityDefs();
+            }
+
+            return instance;
+        }
+    }
+    public Material GetMaterial(BrickType btype)
+    {
+        Material mat = null;
+        if (!m_materialList.ContainsKey(btype)) 
+        {
+            string mat_str = "Materials/" + m_materialMap [btype];
+            mat = (Material)Resources.Load (mat_str);
+            if (mat == null) 
+            {
+                Debug.Log ("Material not found " + mat_str);
+                return null;
+            }                
+        }  
+        else
+            mat=m_materialList[btype];
+        return mat;
+    }    
+}
+
+
+
