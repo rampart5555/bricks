e23f0fd79fdafbb7d229fcb1afc20cf54a4f7876
U-DESKTOP-FLC9L1M\dcioata <dcioata@luxoft.com>
2018-07-04 17:32:02 +0300

    Added main menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonUp(0)) 
+        {                        
+            //Debug.Log ("Mouse click");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast (ray, out hit)) 
+            {                   
+                Debug.Log(hit.transform.gameObject.name);
+                SceneManager.LoadScene ("scene", LoadSceneMode.Single);
+                    
+            } 
+        }	
+	}
+}
