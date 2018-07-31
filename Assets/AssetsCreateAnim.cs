56e1dc07dc787a7c2ddba1090ba46a836f093f83
U-DESKTOP-FLC9L1M\dcioata <dcioata@luxoft.com>
2018-07-31 17:24:44 +0300

    added animation

--- Assets/AssetsCreateAnim.cs
+++ Assets/AssetsCreateAnim.cs
@@ -10,11 +10,11 @@ using UnityEditor;
 */
 public struct ControlPoint
 {    
+    
     public float  m_time;
     public float[] vec3;
-    public ControlPoint(float time,float x,float y, float z)
+    public ControlPoint(float time, float x,float y, float z)
     {
-        
         m_time = time;
         vec3 = new float[3];
         vec3[0] = x;
@@ -40,72 +40,59 @@ public class AssetsCreateAnim: MonoBehaviour
             PlayAnimation();
     }
     public void PlayAnimation()
-    {
-        if (m_toggle == false)
-        {
-            AnimationState m_state = m_animation["door_left_open"];
-            /*
-            m_state.time = 0;
-            m_state.speed = 1;
-            */
-            m_animation.Play("door_left_open");         
-
-        }
-        else
-        {
-            AnimationState m_state = m_animation["door_left_close"];
-            /*
-            m_state.time = m_state.length;
-            m_state.speed = -1;
-            */
-            m_animation.Play("door_left_close");         
-
-        }
-        m_toggle = !m_toggle;
+    {       
+        //AnimationState m_state = m_animation["level_start"];
+        m_animation.Play("level_start");         
     }
 
-    public AnimationClip CreateAnim(string clip_name, GameObject go, List<ControlPoint> cp_list)
-    {                        
-        AnimationClip clip = new AnimationClip();
-        clip.legacy = true;
-        clip.name = clip_name;
+    public void CreateAnimationCurve(AnimationClip clip, string obj_name, float[] cp_list)
+    {                                
         string[] axis_pos_str = { "localPosition.x", "localPosition.y", "localPosition.z" };
         for (int i = 0; i < 3; i++)
-        {
-            Keyframe[] keys = new Keyframe[cp_list.Count];
-            for (int j=0;j<cp_list.Count;j++)
+        {            
+            //Keyframe[] keys = new Keyframe[cp_list.Length];
+            //int index = 0;
+            AnimationCurve curve = new AnimationCurve();
+            for (int j = 0; j < cp_list.Length; j+=4)
             {                
-                keys[j] = new Keyframe(cp_list[j].m_time, cp_list[j].vec3[i]);             
-            }
-            AnimationCurve curve = new AnimationCurve(keys);
-            clip.SetCurve(go.name, typeof(Transform), axis_pos_str[i], curve);
-        }
-        return clip;
-
+                curve.AddKey(new Keyframe(cp_list[j],cp_list[j+i+1]));
+            }                
+            clip.SetCurve(obj_name, typeof(Transform), axis_pos_str[i], curve);
+        }       
     }
 
     public void CreateAnimationCurve()
-    {
-        GameObject scene = GameObject.Find("scene_3");
-        GameObject m_doorLeft = GameObject.Find("door_left");
-
-        Vector3 pos = m_doorLeft.transform.position;
-        List<ControlPoint> cp_list = new List<ControlPoint>()
+    {   
+        Dictionary<string,float[]> anim_dict = new Dictionary<string,float[]>();
+        GameObject doorLeft = GameObject.Find("door_left");
+        Vector3 pos = doorLeft.transform.localPosition;
+        float[] door_left_keys = 
         {
-            new ControlPoint( 0.0f, pos.x, pos.y, pos.z),
-            new ControlPoint( 1.0f, pos.x+1.2f, pos.y, pos.z),
-            new ControlPoint( 2.0f, pos.x+1.2f, pos.y+1.5f, pos.z)
+            0.0f, pos.x, pos.y, pos.z,
+            1.0f, pos.x, pos.y+2.7f, pos.z,
+            1.5f, pos.x, pos.y+2.7f, pos.z, 
+            2.0f, pos.x, pos.y, pos.z
         };
-        AnimationClip clip = CreateAnim("door_left_open", m_doorLeft, cp_list);
-        cp_list.Reverse();
-        AnimationClip clip_2 = CreateAnim("door_left_close", m_doorLeft, cp_list);
-        Debug.Log(clip_2.name);
-        // create a new AnimationClip
-        m_animation= scene.GetComponent<Animation>();
-        m_animation.AddClip(clip_2, clip.name);
-        m_animation.AddClip(clip, clip.name);
+        
+        Vector3 go_from = GameObject.Find("paddle_slot_0").transform.localPosition;
+        Vector3 go_to = GameObject.Find("paddle_start_pos").transform.localPosition;
+        float [] paddle_mesh_0_keys={
+            1.0f, go_from.x, go_from.y, go_from.z,
+            1.5f, go_to.x, go_to.y, go_to.z
+        };               
+        anim_dict.Add("door_left", door_left_keys);
+        anim_dict.Add("paddle_mesh_0", paddle_mesh_0_keys);
 
-       
+        AnimationClip clip=new AnimationClip();
+        clip.legacy=true;
+        clip.name="level_start";
+        foreach(KeyValuePair<string, float[]> entry in anim_dict)
+        {
+            CreateAnimationCurve(clip,entry.Key, entry.Value);
+        }
+        m_animation= GetComponent<Animation>();
+        m_animation.AddClip(clip, "level_start");
+
         //AssetDatabase.CreateAsset(clip, "Assets/anim.anim");
         //AssetDatabase.SaveAssets();
 
